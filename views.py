a3c8ec1d1a4df6f276377738c944c7900fa96234
AndBondStyle <andbondstyle@gmail.com>
2019-07-30 23:24:34 +0300

    Views draft

--- views.py
+++ views.py
@@ -1,3 +1,47 @@
 """
 API method implementations live here
 """
+
+
+class UploadView:
+    method = 'images.upload'
+    limiting = ['10 / min', '1000 / day']  # TODO: Think aboud'it
+    schema = {  # TODO: Add more constraints, move schema to settings?
+        'file': {'type': 'string', 'required': True},  # TODO: What about multipart? Custom cerberus type maybe?
+        'rid': {'type': 'string'},  # TODO: What about rid uniqueness?
+        'tags': {
+            'type': 'list',
+            'schema': {'type': 'string'},
+        },
+    }
+
+
+class UpdateView:
+    method = 'images.update'
+    limiting = ['100 / sec']  # TODO: Default 'just-dont-ddos' will do?
+    schema = {
+        'id': {'type': 'string'},
+        'rid': {'type': 'string'},
+        'tags': {
+            'type': 'list',
+            'schema': {'type': 'string'},
+        },
+        'delete': {'type': 'boolean'},
+    }
+
+
+class SearchView:
+    method = 'images.search'
+    limiting = 'just-dont-ddos'
+    schema = {
+        'query': {'type': 'string'},
+        'id': {'type': 'string'},
+        'rid': {'type': 'string'},
+    }
+
+
+class TokenView:
+    admin = True
+    schema = {
+        'rid': {'type': 'string', 'required': True}
+    }
