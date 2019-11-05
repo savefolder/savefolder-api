6e436903742de2d756c20604dad89a6435eba413
AndBondStyle <andbondstyle@gmail.com>
2019-11-05 23:10:20 +0300

    Update default settings to work with docker

--- settings.py
+++ settings.py
@@ -4,11 +4,11 @@ Global settings live here
 
 from core.environ import env
 
-SECRET = env.str('SECRET')
+SECRET = env.str('SECRET', 'super-secret-key')
 HOST = env.str('HOST', '0.0.0.0')
 PORT = env.int('PORT', 80)
 
-REDIS_URL = env.str('REDIS_URL', 'redis://localhost')
-MONGO_URL = env.str('MONGO_URL', 'mongodb://localhost')
+REDIS_URL = env.str('REDIS_URL', 'redis://redis:6379')
+MONGO_URL = env.str('MONGO_URL', 'mongodb://mongo:27017')
 
 TOKEN_EXPIRE = env.int('TOKEN_EXPIRE', 60 * 60 * 24)
